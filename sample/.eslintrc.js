0084264a8480937f34adfb2821a6be3f160a3a82
Thomson Thomas <thomson.thomas@rokt.com>
2026-05-13 21:15:02 -0400

    ci(trunk): include sample react native eslint config

--- sample/.eslintrc.js
+++ sample/.eslintrc.js
@@ -1,4 +1,11 @@
+const path = require('path');
+
 module.exports = {
   root: true,
   extends: '@react-native',
+  parserOptions: {
+    babelOptions: {
+      configFile: path.join(__dirname, 'babel.config.js'),
+    },
+  },
 };
